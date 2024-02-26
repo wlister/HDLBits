68d06d94daa968a279f22e4547ead44b3014260a
novideo <97853114+wlister@users.noreply.github.com>
2024-02-26 10:05:29 -0800

    Update signed_addition_overflow.v

--- Circuits/Combinational Logic/Arithmetic Circuits/Exams ece241 2014 q1c/signed_addition_overflow.v
+++ Circuits/Combinational Logic/Arithmetic Circuits/Exams ece241 2014 q1c/signed_addition_overflow.v
@@ -3,11 +3,12 @@ module top_module (
     input [7:0] b,
     output [7:0] s,
     output overflow
-); 
-    wire [7:0] carry_out;
-    
-    assign overflow = (s[7] && ~a[7] && ~b[7]) || (~s[7] && a[7] && b[7]);
-  	assign s = a + b;
+); //
+    assign s = a + b;
+    assign overflow = (~a[7] & ~b[7] & s[7]) | (a[7] & b[7] & ~s[7]);
+    /* 
+     * In this case for 2s complement, will overflow if:
+     * a[7] and b[7] are 0 (both positive numbers), but sum[7] is 1 (there is a carry in). This is overflow.
+     * a[7] and b[7] are 1 (both negative numbers), but sum[7] is 0 (there is a carry in). This is underflow.
+     */
 endmodule
-
-//could also use generate, but lets not do that...
