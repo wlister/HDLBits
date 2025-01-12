c576f8c631f71a14611c4cfecd5638cc56f429fc
heeheeheeha84 <97853114+wlister@users.noreply.github.com>
2025-01-12 13:05:49 -0800

    Update Bugs mux4.sv

--- Circuits/Bugs mux4.sv
+++ Circuits/Bugs mux4.sv
@@ -8,7 +8,7 @@ module top_module (
     output [7:0] out  ); //
 
     wire mux0, mux1; //need to name these differently from the mux module names
-    mux2 mux0 ( sel[0],    a,    b, mux0 );
+    mux2 mux0 ( sel[0],    a,    b,  );
     mux2 mux1 ( sel[1],    c,    d, mux1 ); //this should have select signal at lowest bit 
     //since this mux is not the one connecting the two together (the next one is)
     mux2 mux2 ( sel[1], mux0, mux1,  out ); //this mux has select signal at bit index 1 
